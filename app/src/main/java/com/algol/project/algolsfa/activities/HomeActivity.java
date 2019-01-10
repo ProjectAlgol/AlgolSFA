786a2cad37604e32302bb02dfe228aae1effda80
unknown <swarnavo.dutta@Pritha-PC.maxmobility.org>
2019-01-10 19:58:22 +0530

    Home

--- app/src/main/java/com/algol/project/algolsfa/activities/HomeActivity.java
+++ app/src/main/java/com/algol/project/algolsfa/activities/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.algol.project.algolsfa.activities;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.algol.project.algolsfa.R;
 
@@ -11,11 +16,32 @@ import com.algol.project.algolsfa.R;
  */
 
 public class HomeActivity extends AppCompatActivity {
+    private Context context;
+    private Toolbar commonToolBar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        context= HomeActivity.this;
+        commonToolBar= findViewById(R.id.tb_common);
+        setSupportActionBar(commonToolBar);
+        ActionBar actionBar= getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.common_toolbar_menu,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
